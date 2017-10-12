f371186e5527d48fa006c123652b408552456a5a
cw <cw@llip.life>
2017-10-11 21:11:55 -0700

    test2.1 again

--- test/test2.1.js
+++ test/test2.1.js
@@ -1,7 +1,15 @@
-const max=(a,b,c)=>{
-  let result = a;
-  result = result>b?result:b;
-  result = result>c?result:c;
-  return result;
+const max = require('../solutions/prob2.js');
+const test=(a,b,c,d)=>{
+  console.log(max(a,b,c));
+  if(max(a,b,c)== d){ 
+    console.log('Pass');
+  }else{
+    console.log('Fail');
+  };
 };
-module.exports = max;
+test(5,8,9,9);
+test(1,2,3,3);
+test(1,2,1,1)
+
+;
+
